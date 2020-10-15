a76beeb7885e1ddfaeb24a75146ad54930ec1844
Esemega <sogaral89@gmail.com>
2020-10-15 23:21:49 +0200

    Adding basic functionality to app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
  /* - La tarifa de la habitación por noche es de:
-    100€ para categoría Standard, 120€ Junior Suite y 150€ Suite.
+    100€ para categoría Standard, 
+    120€ Junior Suite y 
+    150€ Suite.
     - El uso del SPA incrementa la tarifa anterior en 20€.
     - Una vez tengas la tarifa en función de la categoría y el SPA: 
     el uso individual reduce un 25% la tarifa por noche, 
@@ -9,13 +11,31 @@
     - Cada noche de parking suma 10€.
  */
 
- //Get the seleted values 
-const roomType = document.getElementById("room-type").value;
-const haveSpa = document.getElementById("spa").checked;
-const roomOcupation = document.getElementById("room-ocupation").value;
-const numberOfNights = document.getElementById("number-of-nights").value;
-const parkingNights = document.getElementById("parking-nights").value;
+const eventHandler = () => {
+    //initalizing result variable
+    let result;
+    
+    //Get the seleted values 
+    const roomType = document.getElementById("room-type").value;
+    const haveSpa = document.getElementById("spa").checked;
+    const roomOcupation = document.getElementById("room-ocupation").value;
+    const numberOfNights = document.getElementById("number-of-nights").value;
+    const parkingNights = document.getElementById("parking-nights").value;
 
-//Formula
+    //Formula
+    if (roomType === "Standard") result = 100;
+    if (roomType === "Junior Suite") result = 120;
+    if (roomType === "Suite") result = 150;
+
+    if (haveSpa) result = result + 20;
+
+    if (roomOcupation === "single") result = result - result*0.25;
+    else if (roomOcupation === "triple") result = result + result*0.25;
+
+    result = result*numberOfNights + 10*parkingNights;
+
+    document.getElementById("result").innerText = result+" €";
+}
 
 //Add event handler to button
+document.getElementById("calculate").addEventListener("click",eventHandler);
